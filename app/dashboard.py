3f840b5415fb28c3601620d60ede8d600e5a219a
Steven McCauley <steven_mccauley@brown.edu>
2017-06-06 10:59:25 -0400

    Cleanup. On to editing

--- app/dashboard.py
+++ app/dashboard.py
@@ -54,62 +54,62 @@ def editor(kls, frmt):
 	resp = requests.post(query_url, data=data, headers=headers)
 	return '<p>{0}</p>'.format(resp.text.encode('utf-8'))
 
-@app.route('/explore/venues/')
-def venues():
-	query = '''
-	CONSTRUCT { ?s <http://www.w3.org/2000/01/rdf-schema#label> ?label . }
-	WHERE {
-	?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://vivo.brown.edu/ontology/citation#Venue> .
-	?s <http://www.w3.org/2000/01/rdf-schema#label> ?label .
-	}
-	'''
-	headers = {'Accept': 'application/rdf+xml'}
-	data = { 'email': email, 'password': passw, 'query': query }
-	resp = requests.post(query_url, data=data, headers=headers)
-	root = etree.fromstring(resp.text.encode('utf-8'))
-	data = [ ( desc.get('{http://www.w3.org/1999/02/22-rdf-syntax-ns#}about'),
-				desc[0].text ) for desc in root ]
-	venue_data = pd.DataFrame(data, columns=['uri', 'label'])
-	venue_data['rabid'] = venue_data['uri'].apply( lambda x: x[33:] )
-	out = venue_data.sort_values('label').to_records('dict')
-	return render_template('explore.html', data=out)
+@app.route('/explore/')
+def explorer():
+	return render_template('explore.html')
 
-@app.route('/venues/details/<rabid>')
-def venue_details(rabid):
+@app.route('/explorer/<rabid>')
+def explore_details(rabid):
 	uri = 'http://vivo.brown.edu/individual/' + rabid
-	query = '''
-	CONSTRUCT {{ <{0}> ?p1 ?o . 
-	             ?s ?p2 <{0}> . }}
-	WHERE {{
-	<{0}> ?p1 ?o .
-	?s ?p2 <{0}> .
-	}}
+	sbj_query = '''
+	CONSTRUCT {{ <{0}> ?p1 ?o . }}
+	WHERE {{<{0}> ?p1 ?o .}}
+	'''.format(uri)
+	obj_query = '''
+	CONSTRUCT {{ ?s ?p2 <{0}> .}}
+	WHERE{{	?s ?p2 <{0}> . }}
 	'''.format(uri)
 	headers = {'Accept': 'application/rdf+xml'}
-	data = { 'email': email, 'password': passw, 'query': query }
+	
+	data = { 'email': email, 'password': passw, 'query': sbj_query }
 	resp = requests.post(query_url, data=data, headers=headers)
 	tree = etree.fromstring(resp.text.encode('utf-8'))
-	sbj = rdf_finder(tree, uri=uri)[0]
-	arrows_out = defaultdict(list)
-	simple_props = defaultdict(list)
-	for pred in sbj:
+	sbj_data = rdf_finder(tree, uri=uri)
+	if len(sbj_data) < 1:
+		sbj_results = []
+	else:
+		sbj_results = sbj_data[0]
+	pointers = defaultdict(list)
+	simple_data = defaultdict(list)
+	for pred in sbj_results:
 		obj = pred.get('{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource')
 		if obj:
-			arrows_out[pred.tag.translate(None, '{}')].append(obj)
+			pointers[pred.tag.translate(None, '{}')].append(obj)
 		else:
-			simple_props[pred.tag.translate(None, '{}')].append(pred.text)
-	arrows_in = defaultdict(list)
-	others = [ desc for desc in tree
-		if desc.get('{http://www.w3.org/1999/02/22-rdf-syntax-ns#}about') != uri]
-	for o in others:
+			simple_data[pred.tag.translate(None, '{}')].append(pred.text)
+
+	data = { 'email': email, 'password': passw, 'query': obj_query }
+	resp = requests.post(query_url, data=data, headers=headers)
+	tree = etree.fromstring(resp.text.encode('utf-8'))
+	obj_data = rdf_finder(tree, uri=uri)
+	if len(obj_data) < 1:
+		obj_results = []
+	else:
+		obj_results = obj_data[0]
+	targets = defaultdict(list)
+	for o in obj_results:
 		o_sbj = o.get(
 			'{http://www.w3.org/1999/02/22-rdf-syntax-ns#}about').translate(None, '{}')
 		for pred in o:
-			arrows_in[pred.tag.translate(None, '{}')].append(o_sbj)
-	data_in = [ { 'predicate': key, 'subjects': val } for key, val in arrows_in.items() ]
-	data_out = [ { 'predicate': key, 'objects': val } for key, val in arrows_out.items() ]
-	data_props = [ { 'predicate': key, 'value': val } for key, val in simple_props.items() ]
-	return jsonify({'in': data_in, 'out': data_out, 'properties': data_props })
+			targets[pred.tag.translate(None, '{}')].append(o_sbj)
+
+	data_targets = [ { 'predicate': key, 'subjects': val } for key, val in targets.items() ]
+	data_pointers = [ { 'predicate': key, 'objects': val } for key, val in pointers.items() ]
+	data_properties = [ { 'predicate': key, 'value': val } for key, val in simple_data.items() ]
+	return jsonify({'targets': data_targets,
+					'pointers': data_pointers,
+					'properties': data_properties,
+					'uri': uri })
 
 @app.route('/selector/')
 def selector_list():
